61ce71cfbc2411f5e1d245a5f8174d55a37ada7d
Matthew de Queljoe <matthew.dequeljoe@gmail.com>
2018-11-30 11:30:07 +0100

    add raw utils + file formatting

--- src/utils.h
+++ src/utils.h
@@ -7,24 +7,45 @@
 
 using namespace Rcpp;
 
-bool has_element(CharacterVector x, const char* s){
+bool has_element(CharacterVector x, const char *s)
+{
   return std::find(x.begin(), x.end(), s) != x.end();
 }
 
-CharacterVector get_node_attr(rapidxml::xml_node<> *node, CharacterVector names){
+std::string raw_to_string(RawVector x)
+{
+  std::string s(RAW(x), RAW(x) + x.size());
+  return s;
+}
 
-  if (node -> first_attribute() == 0)
+std::string read_binlist(List rawlist)
+{
+  std::string m;
+  for (int i = 0; i < s.size(); i++)
+  {
+    m += raw_to_string(as<RawVector>(s[i]));
+  }
+  m.push_back('\0');
+  return m;
+}
+
+CharacterVector get_node_attr(rapidxml::xml_node<> *node, CharacterVector names)
+{
+
+  if (node->first_attribute() == 0)
     return names;
 
   for (rapidxml::xml_attribute<> *attr = node->first_attribute();
-       attr; attr = attr -> next_attribute()){
+       attr; attr = attr->next_attribute())
+  {
     if (!has_element(names, attr->name()))
       names.push_back(attr->name());
   }
   return names;
 }
 
-List get_node_value(rapidxml::xml_node<> *node, List data, int m){
+List get_node_value(rapidxml::xml_node<> *node, List data, int m)
+{
   for (rapidxml::xml_attribute<> *series_attr = node->first_attribute();
        series_attr; series_attr = series_attr->next_attribute())
   {
@@ -33,21 +54,26 @@ List get_node_value(rapidxml::xml_node<> *node, List data, int m){
   return data;
 }
 
-List fill_series(List data, CharacterVector fill_names, int n, int m){
+List fill_series(List data, CharacterVector fill_names, int n, int m)
+{
   std::string name;
   int i, j;
-  for (i = 0; i < fill_names.size(); i++){
+  for (i = 0; i < fill_names.size(); i++)
+  {
     name = fill_names[i];
-    for (j = n; j < m; j++){
+    for (j = n; j < m; j++)
+    {
       as<CharacterVector>(data[name])[j] = as<CharacterVector>(data[name])[j - 1];
     }
   }
   return data;
 }
 
-List init_dataframe(int n, int v){
+List init_dataframe(int n, int v)
+{
   List out(n);
-  for (int i = 0; i < n; i++){
+  for (int i = 0; i < n; i++)
+  {
     out[i] = CharacterVector(v, NA_STRING);
   }
   return out;
