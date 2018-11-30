463a240742dd122c2faade90a6788950ee09db72
Matthew de Queljoe <matthew.dequeljoe@gmail.com>
2018-11-30 09:47:01 +0100

    add connections

--- src/readsdmx.h
+++ src/readsdmx.h
@@ -5,51 +5,43 @@
 #include "rapidxml.hpp"
 #include <rapidxml_utils.hpp>
 #include <compactdata.h>
+
 using namespace Rcpp;
 
-//to do: add other datamessages
-enum DataMessage {
-  COMPACTDATA,
-  GENERICDATA
-};
-
-class ReadSDMX {
-
-  rapidxml::file<> sdmx_file_;
+class ReadSDMX
+{
   rapidxml::xml_document<> sdmxdoc_;
   rapidxml::xml_node<> *root_;
   DataMessage data_type;
 
 public:
-
-  ReadSDMX(const char* path):sdmx_file_(path)
+  ReadSDMX(std::string filetext)
   {
-
+    sdmxdoc_.parse<rapidxml::parse_strip_xml_namespaces>(&filetext[0]);
+    root_ = sdmxdoc_.first_node();
+    data_type = get_data_message();
+  }
+  ReadSDMX(const char *path)
+  {
+    rapidxml::file<> sdmx_file_(path);
     sdmxdoc_.parse<rapidxml::parse_strip_xml_namespaces>(sdmx_file_.data());
     root_ = sdmxdoc_.first_node();
     data_type = get_data_message();
   }
 
-  List read_sdmx(){
+  List read_sdmx()
+  {
 
     if (data_type == COMPACTDATA)
-      return read_compactdata();
+      return read_sdmxdata<COMPACTDATA>(root_);
     return List(1); /*add in generic format later */
   }
 
 private:
-
-  DataMessage get_data_message(){
-    //data type information can also be stored in dataset node...
-    if (strcmp(root_ -> name(), "CompactData") == 0)
-      return COMPACTDATA;
-    else return GENERICDATA;
+  DataMessage get_data_message()
+  {
+    return data_message_(root_);
   }
-
-  List read_compactdata(){
-    return read_compactdata_(root_);
-  }
-
 };
 
 #endif
