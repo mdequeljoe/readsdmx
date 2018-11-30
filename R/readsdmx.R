9b2e3ff7a29c4680eb3c768e055a0b64137c8d77
Matthew de Queljoe <matthew.dequeljoe@gmail.com>
2018-11-30 15:30:33 +0100

    pass n to readbin + format verbose

--- R/readsdmx.R
+++ R/readsdmx.R
@@ -1,19 +1,24 @@
-
 #' Read SDMX data
 #'
 #' read SDMX data into R as dataframes
 #'
-#' @param path the path to an SDMX-ML xml file or an url to connect to.
+#' @param path Character; the path to an SDMX-ML xml file or an url to connect
+#'   to.
+#' @param n Integer; if reading data from a connection pass n number of records
+#'   to be read via readBin.
+#' @param verbose Logical; When connecting to large datasets verbose will
+#'   display the progress of the records read as expressed by its object size.
 #' @return data.frame object
 #'
-#' @details read_sdmx returns a dataframe with the SDMX data 'as-is'
-#' which means that all columns will be character vectors.
+#' @details read_sdmx returns a dataframe with the SDMX data 'as-is' which means
+#'   that all columns will be character vectors.
 #'
 #' @export
-read_sdmx <- function(path, verbose = FALSE) {
-
-  if (grepl("^http[s]?://", path)){
-    path <- stream_con(path, verbose)
+read_sdmx <- function(path,
+                      n = 64 * 1024,
+                      verbose = FALSE) {
+  if (grepl("^http[s]?://", path)) {
+    path <- stream_con(path, n, verbose)
     return(read_sdmx_(path))
   }
 
@@ -21,24 +26,29 @@ read_sdmx <- function(path, verbose = FALSE) {
   read_sdmx_(path)
 }
 
-stream_con <- function(con, verbose = FALSE) {
-
+stream_con <- function(con,
+                       n = 64 * 1024,
+                       verbose = FALSE) {
   if (!is(con, "connection"))
     con <- url(con)
   open(con, "rb")
   on.exit(close(con))
   out <- list()
   n <- 1
+  b <- 0
+  class(b) <- "object_size"
 
   repeat {
-    if (verbose)
-      cat("\r reading chunk: ", n)
-    txt <- readBin(con, "raw", n = 64 * 1024)
+    txt <- readBin(con, "raw", n = n)
     if (!length(txt))
       break
+    if (verbose) {
+      b <- b + object.size(txt)
+      cat("\r read from connection: ",
+          format(b, unit = "Mb", standard = "SI"))
+    }
     out[[n]] <- txt
     n <- n + 1
   }
   out
 }
-
