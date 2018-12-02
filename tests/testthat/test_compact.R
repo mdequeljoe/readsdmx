022a5d5c1d7c979226735bac06039f079ca07761
Matthew de Queljoe <matthew.dequeljoe@gmail.com>
2018-12-02 18:01:20 +0100

    + compact tests

--- tests/testthat/test_compact.R
+++ tests/testthat/test_compact.R
@@ -4,7 +4,7 @@ u <-
   "https://stats.oecd.org/restsdmx/sdmx.ashx/GetData/HH_DASH/AUS.RGDP_INDEX.Q/all?startTime=2007-Q1&endTime=2008-Q2&format=compact_v2"
 f <- system.file("tests/testthat/compact_test.xml", package = "readsdmx")
 k <- read_sdmx(u)
-h <- read_sdmx(u)
+h <- read_sdmx(f)
 
 test_that("compact data read", {
   expect_true(identical(h, k))
@@ -13,3 +13,13 @@ test_that("compact data read", {
   expect_true(all(sapply(k, inherits, "character")))
 })
 
+test_that("series with no observations read as NA", {
+  d <- read_sdmx(
+    system.file("tests/testthat/compact_test2.xml", package = "readsdmx")
+  )
+  expect_equal(unique(d$LOCATION), c("FRA", "BEL", "EST"))
+  expect_equal(d[nrow(d), "LOCATION"], "EST")
+  expect_equal(d[nrow(d), "TIME"], NA_character_)
+  expect_equal(d[nrow(d), "OBS_VALUE"], NA_character_)
+
+})
