ea20c2753b6c438ecb37ec5271b3cb05ac60929a
Matthew de Queljoe <matthew.dequeljoe@gmail.com>
2018-12-11 19:36:07 +0100

    use maps for compactdata

--- src/compactdata.h
+++ src/compactdata.h
@@ -3,107 +3,77 @@
 
 #include <Rcpp.h>
 #include "rapidxml.hpp"
-#include <rapidxml_utils.hpp>
-#include "utils.h"
 #include "datamessage.h"
-
 using namespace Rcpp;
 
-List compactdata_content_(rapidxml::xml_node<> *root)
+std::map<std::string, std::string> get_node_value_(rapidxml::xml_node<> *node)
 {
-  int n, nseries = 0, nobs = 0;
-  CharacterVector series_attr, obs_attr, colnames;
-  rapidxml::xml_node<> *dataset = root->first_node("DataSet");
+  std::map<std::string, std::string> out;
+  if (node->first_attribute() == 0)
+    return out;
 
-  for (rapidxml::xml_node<> *series = dataset->first_node("Series");
-       series; series = series->next_sibling())
+  for (rapidxml::xml_attribute<> *series_attr = node->first_attribute();
+       series_attr; series_attr = series_attr->next_attribute())
   {
-    if (strcmp(series -> name(), "Series") != 0)
-      break;
-
-    ++nseries;
-    series_attr = get_node_attr(series, series_attr);
-
-    //series with no obs still returned to dataframe as NA
-    if (series->first_node("Obs") == NULL){
-      ++nobs;
-      continue;
-    }
-    for (rapidxml::xml_node<> *obs = series->first_node("Obs");
-         obs; obs = obs->next_sibling())
-    {
-      ++nobs;
-      obs_attr = get_node_attr(obs, obs_attr);
-    }
+    out[series_attr->name()] = series_attr->value();
   }
-
-  //colnames is series_attributes + obs_attributes
-  colnames = series_attr;
-
-  for (n = 0; n < obs_attr.size(); n++)
-  {
-    colnames.push_back(obs_attr[n]);
-  }
-
-  return List::create(Named("colnames") = colnames,
-                      Named("series") = series_attr,
-                      Named("obs") = obs_attr,
-                      Named("n_series") = nseries,
-                      Named("n_obs") = nobs);
+  return out;
 }
 
 template<>
-List read_sdmxdata<COMPACTDATA>(rapidxml::xml_node<> *root)
+std::map<std::string, CharacterVector> readsdmx<COMPACTDATA>(rapidxml::xml_node<> *root)
 {
-  List content = compactdata_content_(root);
-  CharacterVector series_names = content["series"];
-  CharacterVector colnames = content["colnames"];
-  int nobs = content["n_obs"];
-  int nvars = colnames.size();
-  int m = 0, n;
-
-  //dataframe to return
-  List out = init_dataframe(nvars, nobs);
-  out.attr("names") = colnames;
-
-  //dataset
+  int m = 0;
+  std::vector<std::map<std::string, std::string> > data_;
   rapidxml::xml_node<> *dataset = root->first_node("DataSet");
+  std::map<std::string, std::string> obs_key, obs_val, series_key;
+  std::map<std::string, CharacterVector> out;
 
   //series
   for (rapidxml::xml_node<> *series = dataset->first_node("Series");
        series; series = series->next_sibling())
   {
 
-    //stop after first sibling that isnt a series is reached
-    if (strcmp(series -> name(), "Series") != 0)
+    if (strcmp(series->name(), "Series") != 0)
       break;
 
-    //series attributes
-    out = get_node_value(series, out, m);
-    n = m + 1;
+    series_key = get_node_value_(series);
 
     //series observations
-    //series with no obs still returned to dataframe as NA
-    if (series->first_node("Obs") == NULL)
-    {
-      ++m;
-      continue;
-    }
     for (rapidxml::xml_node<> *obs = series->first_node("Obs");
          obs; obs = obs->next_sibling())
     {
-      //series observation attributes
-      out = get_node_value(obs, out, m);
+      obs_key = series_key;
+      // series with no obs still returned to dataframe as NA
+      if (obs == NULL)
+      {
+        data_.push_back(obs_key);
+        ++m;
+        break;
+      }
+      // //series observation attributes
+      obs_val = get_node_value_(obs);
+      obs_key.insert(obs_val.begin(), obs_val.end());
+      data_.push_back(obs_key);
       ++m;
     }
-
-    // fill forward series attributes
-    out = fill_series(out, series_names, n, m);
-
   }
 
-  out.attr("row.names") = seq(1, nobs);
-  out.attr("class") = "data.frame";
+
+  for (std::size_t i = 0; i < data_.size(); i++)
+  {
+    std::map<std::string, std::string> d = data_[i];
+    for (std::map<std::string, std::string>::iterator it_ = d.begin(); it_ != d.end(); ++it_)
+    {
+      std::string nm = it_->first;
+      if (out.find(nm) == out.end())
+      {
+        out[nm] = CharacterVector(m, NA_STRING);
+      }
+      out[nm][i] = it_->second;
+    }
+  }
+
   return out;
 }
 
