12fe1286c9cb7d13e3c43b81e21cfed3b379a9ff
Matthew de Queljoe <matthew.dequeljoe@gmail.com>
2018-12-13 10:56:25 +0100

    update compact test

--- tests/testthat/test_compact.R
+++ tests/testthat/test_compact.R
@@ -1,25 +1,26 @@
 context("compactdata")
 
-u <-
-  "https://stats.oecd.org/restsdmx/sdmx.ashx/GetData/HH_DASH/AUS.RGDP_INDEX.Q/all?startTime=2007-Q1&endTime=2008-Q2&format=compact_v2"
-f <- system.file("tests/testthat/compact_test.xml", package = "readsdmx")
-k <- read_sdmx(u)
+u <- readRDS(system.file("extdata/compact_con.RDS", package = "readsdmx"))
+k <- as.data.frame(read_sdmx_connection_(u), stringsAsFactors = FALSE)
+f <- system.file("extdata/compact_2.0.xml", package = "readsdmx")
 h <- read_sdmx(f)
 
-test_that("compact data read", {
+test_that("connection and local file produces same output", {
   expect_true(identical(h, k))
-  expect_equal(nrow(k), 6)
-  expect_equal(ncol(k), 8)
-  expect_true(all(sapply(k, inherits, "character")))
+})
+
+test_that("compact data read", {
+  expect_equal(nrow(h), 6)
+  expect_equal(ncol(h), 8)
+  expect_true(all(sapply(h, inherits, "character")))
 })
 
 test_that("series with no observations read as NA", {
   d <- read_sdmx(
-    system.file("tests/testthat/compact_test2.xml", package = "readsdmx")
+    system.file("extdata/compact_nullobs_2.0.xml", package = "readsdmx")
   )
   expect_equal(unique(d$LOCATION), c("FRA", "BEL", "EST"))
   expect_equal(d[nrow(d), "LOCATION"], "EST")
   expect_equal(d[nrow(d), "TIME"], NA_character_)
   expect_equal(d[nrow(d), "OBS_VALUE"], NA_character_)
-
 })
