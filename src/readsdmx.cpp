b45bcf7b60b6580280ed5f32c1853ffd5c866bb4
Matthew de Queljoe <matthew.dequeljoe@gmail.com>
2018-12-14 11:10:30 +0100

    return data

--- src/readsdmx.cpp
+++ src/readsdmx.cpp
@@ -1,17 +1,16 @@
 #include <Rcpp.h>
 #include <SDMXReader.h>
 
-
 // [[Rcpp::export]]
 std::map<std::string, Rcpp::CharacterVector> read_sdmx_(std::string fname)
 {
   SDMXReader d(fname);
-  return d.read_sdmx();
+  return d.data();
 }
 
 // [[Rcpp::export]]
 std::map<std::string, Rcpp::CharacterVector> read_sdmx_connection_(List bdata)
 {
   SDMXReader d(bdata);
-  return d.read_sdmx();
+  return d.data();
 }
