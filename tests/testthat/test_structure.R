60337f1d0341e944b0f1a8d282ef3227be86165f
Matthew de Queljoe <matthew.dequeljoe@gmail.com>
2018-12-26 23:07:12 +0100

    add strdata tests

---
+++ tests/testthat/test_structure.R
@@ -0,0 +1,30 @@
+context("structuredata")
+
+test_that("version 2.0 structure data is read", {
+  d <-
+    read_sdmx(system.file("extdata/codelist_2.0.xml", package = "readsdmx"))
+  expect_true(all(sapply(d, inherits, "character")))
+  expect_true(all(
+    colnames(d) %in%
+      c("en", "en_description", "fr", "fr_description", "id", "value")
+  ))
+  expect_equal(d$en_description[[1]], "Australia")
+  expect_equal(d$fr_description[[1]], "Australie")
+})
+
+test_that("version 2.1 structure data is read", {
+  d <-
+    read_sdmx(system.file("extdata/codelist_2.1.xml", package = "readsdmx"))
+  expect_equal(d$en[[1]], "CL_FAO_MAJOR_AREA")
+  expect_true(all(sapply(d, inherits, "character")))
+  expect_true(all(
+    colnames(d) %in% c(
+      "en",
+      "en_description",
+      "es_description",
+      "fr_description",
+      "id",
+      "id_description"
+    )
+  ))
+})
