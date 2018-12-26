fee6657dd34c92072a997e8ce757b40d59b7f671
Matthew de Queljoe <matthew.dequeljoe@gmail.com>
2018-12-26 17:43:30 +0100

    add structuredata for 2.0

--- src/datamessage.h
+++ src/datamessage.h
@@ -74,4 +74,24 @@ DataMessage
     return find_data_message(dataset, char(':'));
   }
 
+std::map<std::string, Rcpp::CharacterVector> as_list(std::vector<std::map<std::string, std::string> > data, int len)
+{
+  std::map<std::string, Rcpp::CharacterVector> out;
+  for (std::size_t i = 0; i < data.size(); i++)
+  {
+    std::map<std::string, std::string> d = data[i];
+    for (std::map<std::string, std::string>::iterator it_ = d.begin(); it_ != d.end(); ++it_)
+    {
+      std::string nm = it_->first;
+      if (out.find(nm) == out.end())
+      {
+        out[nm] = Rcpp::CharacterVector(len, NA_STRING);
+      }
+      out[nm][i] = it_->second;
+    }
+  }
+  return out;
+}
+
+
 #endif
--- src/structuredata.h
+++ src/structuredata.h
@@ -4,42 +4,17 @@
 #include "rapidxml.h"
 #include "datamessage.h"
 
-//<CodeList id="CL_QNA_LOCATION" agencyID="OECD">
-// <Name xml:lang="en">Country</Name>
-// <Name xml:lang="fr">Pays</Name>
-//
-// id en fr
-// cl_qna, country, pays// <Code value="AUS">
-// <Description xml:lang="en">Australia</Description>
-// <Description xml:lang="fr">Australie</Description>
-// </Code>
-
-// <message:CodeLists>
-//   <CodeList id="CL_THE_CODELIST" agencyID="ORG">
-//   <Name xml:lang="en">the codelist</Name>
-//   <Name xml:lang="fr">la liste de codes</Name>
-//   <Code value="A">
-//   <Description xml:lang="en">A letter</Description>
-//   <Description xml:lang="fr">lettre A</Description>
-//   </Code>
-
-// id               country   pays
-// CL_QNA_LOCATION Australia Australie
-
 
 std::map<std::string, std::string> cl_key_(rapidxml::xml_node<> *cl_node, const char* attr_name, const char* node_name){
 
   std::map<std::string, std::string> cl;
-  cl["id"] = cl_node -> first_attribute(attr_name) -> value();
+  cl[attr_name] = cl_node -> first_attribute(attr_name) -> value();
   for (rapidxml::xml_node<> *val = cl_node->first_node(node_name);
        val; val = val->next_sibling())
   {
     if (strcmp(val->name(), node_name) != 0)
       break;
-
     rapidxml::xml_attribute<> *cl_lang = val->first_attribute("lang");
-    Rcpp::Rcout << val->value() << std::endl;
-    Rcpp::Rcout << cl_lang->value() << std::endl;
     cl[cl_lang->value()] = val->value();
   }
   return cl;
@@ -48,10 +23,16 @@ std::map<std::string, std::string> cl_key_(rapidxml::xml_node<> *cl_node, const 
 template <>
 std::map<std::string, Rcpp::CharacterVector> readsdmx<STRUCTUREDATA>(rapidxml::xml_node<> *root)
 {
-  rapidxml::xml_node<> *codelists = root->first_node("CodeLists");
+  // rapidxml::xml_node<> *codelists = root->first_node("CodeLists");
+  // if (codelists == NULL){
+  //   codelists = root->first_node("Structures")->first_node("CodeLists");
+  // }
+  // if (codelists == NULL)
+  //   Rcpp::stop("codelists not found");
+  rapidxml::xml_node<> *codelists = (root->first_node("CodeLists") == NULL) ?
+    root->first_node("Structures")->first_node("Codelists") : root->first_node("CodeLists");
   if (codelists == NULL)
     Rcpp::stop("codelists not found");
-  std::map<std::string, Rcpp::CharacterVector> out;
   std::vector<std::map<std::string, std::string> > data_;
   std::map<std::string, std::string> code_list_key, code_key, cl_data;
   int m = 0;
@@ -70,25 +51,18 @@ std::map<std::string, Rcpp::CharacterVector> readsdmx<STRUCTUREDATA>(rapidxml::x
         break;
       code_key = cl_key_(code, "value", "Description");
       cl_data = code_list_key;
-      cl_data.insert(code_key.begin(), code_key.end());
+
+      for (std::map<std::string, std::string>::iterator it_ = code_key.begin();
+           it_ != code_key.end(); ++it_)
+      {
+        std::string nm = (it_->first.compare("value") == 0) ? it_->first : it_->first + "_description";
+        cl_data[nm] = it_ -> second;
+      }
       data_.push_back(cl_data);
       ++m;
     }
   }
-  for (std::size_t i = 0; i < data_.size(); i++)
-  {
-    std::map<std::string, std::string> d = data_[i];
-    for (std::map<std::string, std::string>::iterator it_ = d.begin(); it_ != d.end(); ++it_)
-    {
-      std::string nm = it_->first;
-      if (out.find(nm) == out.end())
-      {
-        out[nm] = Rcpp::CharacterVector(m, NA_STRING);
-      }
-      out[nm][i] = it_->second;
-    }
-  }
-
+  std::map<std::string, Rcpp::CharacterVector> out = as_list(data_, m);
   return out;
 }
 
