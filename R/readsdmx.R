1de259322f797365532e76574c32d8fd0576a8a3
matthew <matthew.dequeljoe@gmail.com>
2019-01-08 16:05:16 +0100

    use download.file

--- R/readsdmx.R
+++ R/readsdmx.R
@@ -6,16 +6,12 @@ NULL
 #'
 #' read SDMX data into R as dataframes
 #'
-#' @param path Character; the path to an SDMX-ML xml file or an url to connect
-#'   to.
-#' @param n Integer; if reading data from a connection pass n number of records
-#'   to be read via readBin.
-#' @param verbose Logical; When connecting to large datasets verbose will
-#'   display the progress of the records read as expressed by its object size.
+#' @param path Character; the path to an SDMX-ML xml file
+#' @inheritParams utils::download.file
 #' @return data.frame object
-#'
 #' @details read_sdmx returns a dataframe with the SDMX data 'as-is' which means
-#'   that all columns will be character vectors.
+#'   that all columns will be character vectors. The base R function `download.file` is used when
+#'   a url is provided to read_sdmx.
 #'
 #' @examples
 #' \dontrun{
@@ -26,49 +22,25 @@ NULL
 #' f <- system.file("extdata/compact_2.0.xml", package = "readsdmx")
 #' d <- readsdmx::read_sdmx(f)
 #' }
+#' @importFrom utils download.file
 #' @export
-read_sdmx <- function(path,
-                      n = 64 * 1024,
-                      verbose = FALSE) {
-  if (is_url(path)) {
-    d <- read_sdmx_connection(path, n, verbose)
-    return(as.data.frame(d, stringsAsFactors = FALSE))
-  }
-  stopifnot(file.exists(path))
-  path <- normalizePath(path)
-  d <- read_sdmx_(path)
-  as.data.frame(d, stringsAsFactors = FALSE)
-}
-
-read_sdmx_connection <- function(con, n, verbose) {
-  con <- stream_con(con, n, verbose)
-  stopifnot(!!length(con))
-  read_sdmx_connection_(con)
-}
-
-#' @importFrom utils object.size
-stream_con <- function(con,
-                       n = 64 * 1024,
-                       verbose = FALSE) {
-  if (!inherits(con, "connection"))
-    con <- url(con)
-  open(con, "rb")
-  on.exit(close(con))
-  out <- list()
-  n <- 1
-  b <- 0
-  repeat {
-    txt <- readBin(con, "raw", n = n)
-    if (!length(txt))
-      break
-    if (verbose) {
-      b <- b + object.size(txt)
-      cat("\rread from connection:", round(b / 1e6, 2), "MB")
+read_sdmx <-
+  function(path,
+           destfile = tempfile(fileext = ".xml"),
+           quiet = TRUE,
+           method = "curl",
+           mode = "wb",
+           ...) {
+    if (is_url(path)) {
+      download.file(path, destfile, method, quiet, mode, ...)
+      path <- destfile
     }
-    out[[n]] <- txt
-    n <- n + 1
+    stopifnot(file.exists(path))
+    path <- normalizePath(path)
+    d <- read_sdmx_(path)
+    as.data.frame(d, stringsAsFactors = FALSE)
   }
-  out
-}
 
-is_url <- function(d) grepl("^http[s]?://", d)
+
+is_url <- function(d)
+  grepl("^http[s]?://", d)
--- tests/testthat/test_compact.R
+++ tests/testthat/test_compact.R
@@ -1,18 +1,12 @@
 context("compactdata")
 
-u <- readRDS(system.file("extdata/compact_con.RDS", package = "readsdmx"))
-k <- as.data.frame(read_sdmx_connection_(u), stringsAsFactors = FALSE)
-f <- system.file("extdata/compact_2.0.xml", package = "readsdmx")
-h <- read_sdmx(f)
-
-test_that("connection and local file produces same output", {
-  expect_true(identical(h, k))
-})
-
 test_that("compact data read", {
-  expect_equal(nrow(h), 6)
-  expect_equal(ncol(h), 8)
-  expect_true(all(vapply(h, inherits, logical(1), "character")))
+  d <- read_sdmx(
+    system.file("extdata/compact_2.0.xml", package = "readsdmx")
+  )
+  expect_equal(nrow(d), 6)
+  expect_equal(ncol(d), 8)
+  expect_true(all(vapply(d, inherits, logical(1), "character")))
 })
 
 test_that("series with no observations read as NA", {
